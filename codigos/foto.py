7c0a65d3c513933d2192f7aa04598429657bff01
MrChayote <152816326+MrChayote@users.noreply.github.com>
2026-06-25 21:11:39 -0600

    Update foto.py

--- codigos/foto.py
+++ codigos/foto.py
@@ -3,18 +3,15 @@ import cv2
 def tomar_foto():
     # Iniciar la cámara (el 0 indica la cámara predeterminada de tu equipo)
     camara = cv2.VideoCapture(1)
-    
-    # Comprobar si la cámara se abrió correctamente
+
     if not camara.isOpened():
         print("Error: No se pudo acceder a la cámara.")
         return
 
     print("Tomando foto...")
     
-    # Capturar un cuadro (frame) de la cámara
     resultado, imagen = camara.read()
 
-    # Si se capturó correctamente, la guardamos
     if resultado:
         nombre_archivo = "mi_foto.png"
         cv2.imwrite(nombre_archivo, imagen)
@@ -25,5 +22,4 @@ def tomar_foto():
     # IMPORTANTE: Liberar la cámara para que otros programas puedan usarla
     camara.release()
 
-# Ejecutar la función
 tomar_foto()
